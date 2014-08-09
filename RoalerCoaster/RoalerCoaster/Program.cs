ab61705710853c27e0def7fb34a174a5d4fd45ac
Payam <payam49er@gmail.com>
2014-08-09 10:47:50 -0400

    Solution to the Roller Coaster
    https://www.codeeval.com/open_challenges/156/

---
+++ RoalerCoaster/RoalerCoaster/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace RoalerCoaster
+{
+    class Program
+    {
+        public static void MakeRollerCoaster(string path)
+        {
+            using (StreamReader reader = File.OpenText(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var readLine = reader.ReadLine();
+                    if (readLine != null)
+                    {
+                        var characters = readLine.ToCharArray().ToList();
+                        bool switchVal = true;
+                        foreach (var character in characters)
+                        {
+
+                            var isletter = character.ToString().Any(x => char.IsLetter(x));
+                            if (isletter)
+                            {
+                                if (switchVal)
+                                {
+                                    Console.Write(character.ToString().ToUpper());
+                                    switchVal = false;
+                                }
+
+                                else
+                                {
+                                    Console.Write(character.ToString().ToLower());
+                                    switchVal = true;
+                                }
+                            }
+                            else
+                                Console.Write(character);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert the path to the file:");
+            var path = Console.ReadLine();
+            MakeRollerCoaster(path);
+
+            Console.Read();
+        }
+    }
+}
+
