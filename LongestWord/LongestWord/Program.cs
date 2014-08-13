c048dc42940bf06a3da9bad1f6eb48e6204327b2
Payam <payam49er@gmail.com>
2014-08-13 01:37:09 -0400

    solution to the longest word in codeeval https://www.codeeval.com/open_challenges/111/

---
+++ LongestWord/LongestWord/Program.cs
@@ -0,0 +1,49 @@
+﻿///solution to https://www.codeeval.com/open_challenges/111/
+using System;
+using System.IO;
+using System.Linq;
+
+namespace LongestWord
+{
+    class Program
+    {
+        public static void FindLongestWord(string path)
+        {
+
+            using (StreamReader reader = File.OpenText(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var lineVal = reader.ReadLine();
+                    if (lineVal != null)
+                    {
+                        var words = lineVal.Trim().Split(' ').ToList();
+                        string longestWord = null;
+                        int longestWordSize = 0;
+                        foreach (var word in words)
+                        {
+                            if (word.Length > longestWordSize)
+                            {
+                                longestWord = word;
+                                longestWordSize = longestWord.Length;
+                            }
+
+                        }
+
+                        Console.WriteLine(longestWord);
+                    }
+                }
+            }
+        }
+
+
+        static
+            void Main(string[] args)
+        {
+            Console.WriteLine("Enter path to the file containing the texts:");
+            var path = Console.ReadLine();
+            FindLongestWord(path);
+            Console.Read();
+        }
+    }
+}
