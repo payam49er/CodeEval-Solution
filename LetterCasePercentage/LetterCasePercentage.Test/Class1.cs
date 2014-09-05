ba77b828d30b62abce9ec53ee97d345fa0ad3e65
Payam <payam49er@gmail.com>
2014-09-05 01:20:07 -0400

    solution to Letter case percentage https://www.codeeval.com/open_challenges/147/

---
+++ LetterCasePercentage/LetterCasePercentage.Test/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LetterCasePercentage.Test
+{
+    [TestFixture]
+    public class LetterCaseTest
+    {
+        [Test]
+        public void TestThePercentageOfLowerAndUpperCaseLetters()
+        {
+            LetterPercentage letterPercentage = new LetterPercentage();
+
+            var lowerPercentage = letterPercentage.CalculatePercentage("thisTHIS");
+           
+            Assert.AreEqual(50, lowerPercentage);
+        }
+    }
+}
---
+++ LetterCasePercentage/LetterCasePercentage/LetterPercentage.cs
@@ -0,0 +1,38 @@
+﻿///Solution to https://www.codeeval.com/open_challenges/147/
+using System;
+using System.IO;
+
+namespace LetterCasePercentage
+{
+    public class LetterPercentage
+    {
+        public void CalculatePercentage(string path)
+        {
+            using (StreamReader reader = File.OpenText(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        var countLowerCase = 0;
+
+                        for (int i = 0; i < line.Length; i++)
+                        {
+                            if (char.IsLower(line[i]))
+                            {
+                                countLowerCase++;
+                            }
+                        }
+
+                        double lowerPercentage = (countLowerCase / (double)(line.Length)) * 100;
+                        double upperPercentage = 100 - lowerPercentage;
+
+                        Console.WriteLine("lowercase: {0:f2}" + " " + "uppercase: {1:f2}", lowerPercentage, upperPercentage);
+                    }
+                }
+            }
+        }
+    }
+}
+
