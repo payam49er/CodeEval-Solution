62b801e26afc79a2a7eb66a47fd6c15bdf69d54a
Payam <payam49er@gmail.com>
2014-09-06 11:10:27 -0400

    Capitalize first letter of each word in a sentence  https://www.codeeval.com/open_challenges/93/

---
+++ CapitaliazeFirstLetter/CapitaliazeFirstLetter/Program.cs
@@ -0,0 +1,48 @@
+﻿//solution to challenge https://www.codeeval.com/open_challenges/93/u
+using System;
+using System.IO;
+using System.Text;
+
+namespace CapitaliazeFirstLetter
+{
+    class Program
+    {
+        public static void Capitalize(string path)
+        {
+            using (StreamReader reader = File.OpenText(path))
+            {
+                while (!reader.EndOfStream)
+                {
+
+
+                    var lineValue = reader.ReadLine();
+
+                    if (lineValue != null)
+                    {
+                        var words = lineValue.Split(' ');
+                        foreach (var word in words)
+                        {
+                            StringBuilder newWord = new StringBuilder();
+                            newWord.Append(word[0].ToString().ToUpper());
+                            for (int i = 1; i < word.Length; i++)
+                            {
+                                newWord.Append(word[i]);
+                            }
+                            Console.Write(newWord + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the path to the file:");
+            var path = Console.ReadLine();
+            Capitalize(path);
+
+            Console.Read();
+        }
+    }
+}
