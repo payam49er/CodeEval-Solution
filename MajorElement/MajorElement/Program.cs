eb3cf20901860b0a4677e7dccaf39a89817cd990
Payam <payam49er@gmail.com>
2014-09-11 23:48:11 -0400

    The Major Element solution https://www.codeeval.com/open_challenges/132/

---
+++ MajorElement/MajorElement/Program.cs
@@ -0,0 +1,36 @@
+﻿///Solution to CodeEval challenge https://www.codeeval.com/open_challenges/132/
+using System;
+using System.IO;
+using System.Linq;
+
+namespace MajorElement
+{
+    class Program
+    {
+        public static void FindMajorElement(string path)
+        {
+            using (StreamReader reader = File.OpenText(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var lineValue = reader.ReadLine();
+                    if (lineValue != null)
+                    {
+                        var values = lineValue.Split(',');
+                        var result = values.GroupBy(x => x).SingleOrDefault(x => x.Count() >= values.Length / 2);
+                        Console.WriteLine(result != null ? result.Key : "None");
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert the path to the file containing data:");
+            var path = Console.ReadLine();
+            FindMajorElement(path);
+
+            Console.Read();
+        }
+    }
+}
