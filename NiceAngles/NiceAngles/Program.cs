847084878463c91338f71e1c00284da27f8b6ea4
Payam <payam49er@gmail.com>
2014-09-15 14:44:56 -0400

    Better revision that passes CodeEval server.

--- NiceAngles/NiceAngles/Program.cs
+++ NiceAngles/NiceAngles/Program.cs
@@ -10,30 +10,53 @@ namespace NiceAngles
         {
             using (StreamReader reader = File.OpenText(path))
             {
+
+
                 while (!reader.EndOfStream)
                 {
                     var degreeValues = reader.ReadLine();
-                    if (degreeValues != null)
+                    if (!string.IsNullOrWhiteSpace(degreeValues))
                     {
                         var totalVal = degreeValues.Split('.');
+                        if (totalVal.Length == 1)
+                        {
+                            var temp = totalVal[0] + ".0";
+                            totalVal = temp.Split('.');
+                        }
                         var intPart = totalVal[0];
-                        var minutes = Convert.ToDecimal("0." + totalVal[1])*60;
-                        var remainingDecimal = minutes.ToString().Split('.');
-                        var minutePart =(int) minutes;
-                        var seconds = Convert.ToDecimal("0." + remainingDecimal[1])*60;
-                        var secondsPart = (int)seconds;
-                        var report = string.Format("{0}"+"."+"{1:00}"+"'"+"{2:00}"+"''",intPart,minutePart,secondsPart);
+                        var decimalPart =  "0." + totalVal[1];
+                        decimal minutes;
+                        decimal.TryParse(decimalPart, out minutes);
                        
+                        minutes = decimal.Multiply(minutes, 60);
+                        var minutePart = (int)minutes;
+                        var remainingDecimal = minutes.ToString().Split('.');
+                        if (remainingDecimal.Length == 1)
+                        {
+                            var tempDecimal = remainingDecimal[0] + ".0";
+                            remainingDecimal = tempDecimal.Split('.');
+                        }
+                        
+                        
+                        decimal seconds;
+                        decimal.TryParse("0." + remainingDecimal[1], out seconds);
+                        seconds = seconds*60;
+                        var secondsPart = (int) seconds;
+                        var report = string.Format("{0}"+"."+"{1:00}"+"'"+"{2:00}"+'"', intPart,
+                            minutePart, secondsPart);
+
                         Console.WriteLine(report);
                     }
                 }
             }
         }
 
-        static void Main(string[] args)
+        static
+            void Main(string[] args)
         {
-            Console.WriteLine("Please insert the path to the file containing data:");
-            var path = Console.ReadLine();
+            //Console.WriteLine("Please insert the path to the file containing data:");
+            //var path = Console.ReadLine();
+            var path = @"C:\Users\payam\Documents\seconds.txt";
             ConvertToMinSec(path);
 
             Console.Read();
