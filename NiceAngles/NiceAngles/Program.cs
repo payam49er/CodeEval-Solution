9a6b43dbd588ae7f50bf29c6da183e2a11f6406b
Payam <payam49er@gmail.com>
2014-09-18 09:40:22 -0400

    removed the path to local file.

--- NiceAngles/NiceAngles/Program.cs
+++ NiceAngles/NiceAngles/Program.cs
@@ -76,10 +76,9 @@ namespace NiceAngles
         static
             void Main(string[] args)
         {
-           // Console.WriteLine("Please insert the path to the file containing data:");
-           // var path = Console.ReadLine();
+            Console.WriteLine("Please insert the path to the file containing data:");
+            var path = Console.ReadLine();
 
-            var path = @"C:\Users\payam\Documents\seconds.txt";
            
             ConvertToMinSec(path);
 
