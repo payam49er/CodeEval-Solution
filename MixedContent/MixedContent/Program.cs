eaf84a6dd5326678708d68df3d31a623918c658f
Payam <payam49er@gmail.com>
2014-09-18 08:45:31 -0400

    Mixed Content solution https://www.codeeval.com/open_challenges/115/

---
+++ MixedContent/MixedContent/Program.cs
@@ -0,0 +1,48 @@
+﻿///Solution to CodeEval challenge Mixed Contetnt https://www.codeeval.com/open_challenges/115/
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MixedContent
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+
+                    StringBuilder numbers = new StringBuilder();
+                    StringBuilder letters = new StringBuilder();
+
+                    var lineValue = reader.ReadLine();
+                    if (!string.IsNullOrEmpty(lineValue))
+                    {
+                        var strings = lineValue.Split(',');
+                        foreach (var s in strings)
+                        {
+                            if (s.ToCharArray().Any(x => char.IsLetter(x)))
+                            {
+                                letters.Append(s + ",");
+                            }
+                            else
+                            {
+                                numbers.Append(s + ",");
+                            }
+                        }
+                    }
+
+                    Console.Write(letters.ToString().TrimEnd(','));
+                    if (letters.Length != 0 && numbers.Length != 0)
+                        Console.Write("|");
+
+                    Console.Write(numbers.ToString().TrimEnd(','));
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
