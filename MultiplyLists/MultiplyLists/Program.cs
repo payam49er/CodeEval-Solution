88721ef1bee2816da1d37e12b950da35f56982e1
Payam <payam49er@gmail.com>
2014-09-20 16:14:37 -0400

    Multiply lists https://www.codeeval.com/open_challenges/113/

---
+++ MultiplyLists/MultiplyLists/Program.cs
@@ -0,0 +1,50 @@
+﻿///solution to CodeEval challenge of Multiplying lists https://www.codeeval.com/open_challenges/113/
+using System;
+using System.IO;
+using System.Linq;
+
+namespace MultiplyLists
+{
+    class Program
+    {
+        public static void Multiply(string path)
+        {
+            using (StreamReader reader = File.OpenText(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var lineValue = reader.ReadLine();
+                    if (!string.IsNullOrEmpty(lineValue))
+                    {
+                        var values = lineValue.Split('|');
+                        var leftValues = values[0].Trim().Split(' ');
+                        var rightValues = values[1].Trim().Split(' ');
+                        for (int i = 0; i < leftValues.Count(); i++)
+                        {
+                            int valLeft;
+                            int.TryParse(leftValues[i], out valLeft);
+
+                            int valRight;
+                            int.TryParse(rightValues[i], out valRight);
+
+                            Console.Write(valLeft*valRight + " ");
+                        }
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert the path to the file containing the data:");
+            var path = Console.ReadLine();
+            Multiply(path);
+
+            Console.Read();
+        }
+    }
+}
