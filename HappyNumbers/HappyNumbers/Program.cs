81239ff10a90125a9bc8c725a510617582c309c1
Payam <payam49er@gmail.com>
2014-09-21 14:22:44 -0400

    Happy Numbers https://www.codeeval.com/open_challenges/39/

---
+++ HappyNumbers/HappyNumbers/Program.cs
@@ -0,0 +1,52 @@
+﻿///Solution to CodeEval challenge Happy Numbers https://www.codeeval.com/open_challenges/39/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HappyNumbers
+{
+    class Program
+    {
+        public static void FindHappyNumber(string path)
+        {
+            using (StreamReader reader = File.OpenText(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var lineValue = reader.ReadLine();
+                    if (!string.IsNullOrEmpty(lineValue))
+                    {
+                        Console.WriteLine(GetSqaureValue(lineValue)? "1":"0");
+                    }
+                }
+            }
+        }
+
+        private static bool GetSqaureValue(string number)
+        {
+            if (number == "1")
+                return true;
+            if (number == "4")
+                return false;
+
+            List<double> values = new List<double>();
+            for (int i = 0; i < number.Length; i++)
+            {
+                int num;
+                int.TryParse(number[i].ToString(), out num);
+                values.Add(Math.Pow(num, 2));
+            }
+           return GetSqaureValue(values.Sum().ToString());
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert the path to your file containing data:");
+            var path = Console.ReadLine();
+            FindHappyNumber(path);
+
+            Console.Read();
+        }
+    }
+}
