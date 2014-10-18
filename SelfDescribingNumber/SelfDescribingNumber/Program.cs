f9b06edc46dd6c710544d48b42bcf52842ceefd9
Payam <payam49er@gmail.com>
2014-10-18 01:27:09 -0400

    Self Describing number https://www.codeeval.com/open_challenges/40/

---
+++ SelfDescribingNumber/SelfDescribingNumber/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SelfDescribingNumber
+{
+    class Program
+    {
+        public static void SelfDecriptive(string path)
+        {
+            using (StreamReader reader = File.OpenText(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var lineValue = reader.ReadLine();
+
+                    var len = lineValue.Count();
+                    bool isDescriptive = true;
+                    if (lineValue[len-1] == '0')
+                    {
+                        for (int i = 0; i < len; i++)
+                        {
+                            int digit;
+                            int.TryParse(lineValue[i].ToString(), out digit);
+                            var pos = lineValue.Count(x => x.ToString() == i.ToString());
+                            if (digit != pos)
+                            {
+                                isDescriptive = false;
+                                break;
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        isDescriptive = false;
+                    }
+                    Console.WriteLine(isDescriptive? "1" : "0");
+                }
+            }
+        }
+
+        static void Main ( string[] args )
+        {
+            Console.WriteLine("Please insert the path to the file containing the data:");
+            var path = Console.ReadLine();
+            SelfDecriptive(path);
+
+            Console.Read();
+        }
+    }
+}
